142b5660631d1249b688aa2d33725ee7e4034281
Lucas Mancebo <lucmancebo15@gmail.com>
2025-10-25 00:21:25 -0300

    feat: Criação da lógica para criação de qr code Para criação do qr code, utilizei a dependencia npm qrcode-terminal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import prompt from "prompt";
 import mainprompt from "./prompts/prompt-main.js"
+import createQRCode from "./services/qr-code/create.js";
 
 (async function main() {
     prompt.get(mainprompt, async (err, choose) => {
-        if(choose.select == 1) console.log("Escolheu QRcode")
+        if(choose.select == 1) await createQRCode()
         if(choose.select == 2) console.log("Escolheu Password")
     })
+
+    prompt.start()
 })()
