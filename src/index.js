037e4215093bbfb46714c3991b8ff19f5ec7863b
Lucas Mancebo <lucmancebo15@gmail.com>
2025-10-25 01:20:36 -0300

    feat: finalização do projeto com lógica completa e estrutura modularizada alguns pontos de arquitetura do projeto  e refatoração foram feitos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import prompt from "prompt";
-import mainprompt from "./prompts/prompt-main.js"
+import promptSchemaMain from "./prompts-schema/prompt-schema-main.js"
 import createQRCode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 (async function main() {
-    prompt.get(mainprompt, async (err, choose) => {
+    prompt.get(promptSchemaMain, async (err, choose) => {
         if(choose.select == 1) await createQRCode()
-        if(choose.select == 2) console.log("Escolheu Password")
+        if(choose.select == 2) await createPassword()
     })
 
     prompt.start()
--- src/prompts-schema/prompt-schema-qrcode.js
+++ src/prompts-schema/prompt-schema-qrcode.js
@@ -1,6 +1,6 @@
 import chalk from "chalk"
 
-const promptQRCode = [
+const promptSchemaQRCode = [
     {
         name: "link",
         description: chalk.yellow("digite o link para gerar o QRcode")
@@ -14,4 +14,4 @@ const promptQRCode = [
     }
 ]
 
-export default promptQRCode
+export default promptSchemaQRCode
